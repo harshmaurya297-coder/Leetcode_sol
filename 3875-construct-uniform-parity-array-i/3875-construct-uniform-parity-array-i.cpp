b92fb85956669aa475067b5ffd76907f743ca434
Harsh Maurya <harshmaurya297@gmail.com>
2026-09-02 16:03:12 +0530

    Time: 0 ms (100%), Space: 30.3 MB (67.8%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,3 +1,6 @@
+#pragma GCC optimize("Ofast,unroll-loops,fast-math")
+#pragma GCC target("avx,avx2,fma,sse4,popcnt,lzcnt,bmi,bmi2")
+
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
