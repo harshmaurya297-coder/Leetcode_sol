07c6b0e2fe84667c3e3110089c347008f27e0c12
Harsh Maurya <harshmaurya297@gmail.com>
2026-09-03 17:21:13 +0530

    Time: 0 ms (100%), Space: 165.9 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,19 +1,19 @@
+#pragma GCC optimize("Ofast,unroll-loops,fast-math")
+#pragma GCC target("avx,avx2,fma,sse4,popcnt,lzcnt,bmi,bmi2")
+
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int mini_ele = *min_element(nums1.begin(), nums1.end());
-        int even = 0;
-        
+
         int mini_even = INT_MAX, mini_odd = INT_MAX;
-        for(int i = 0; i < nums1.size(); i++){
-            if(nums1[i]%2 == 0){
-                even++;
+        for(int i = 0; i < nums1.size(); i++)
+            if(nums1[i]%2 == 0)
                 mini_even = min(nums1[i], mini_even);
-            }
-            else{
+            
+            else
                 mini_odd = min(nums1[i], mini_odd);
-            }
-        }
+            
+        
 
         if (mini_odd == INT_MAX || mini_odd < mini_even)
             return true;
