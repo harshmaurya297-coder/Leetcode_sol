7e7c0ed97ffe15f8969ffc367d3ec9579be0a435
Harsh Maurya <harshmaurya297@gmail.com>
2026-09-05 20:27:13 +0530

    Time: 8 ms (70.04%), Space: 223.4 MB (7.84%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -1,18 +1,32 @@
 #pragma GCC optimize("Ofast,unroll-loops,fast-math")
 #pragma GCC target("avx,avx2,fma,sse4,popcnt,lzcnt,bmi,bmi2")
+
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> mn(n); // will store mini of i -> n
+        if(n <= 1) return 0;
+        int maxi = nums[0], mini = nums[n - 1];
+        vector<int> mx, v;
+        vector<int> mn(n);
+
+        for(auto it : nums){
+            if(it > maxi)
+                maxi = it;
+            mx.push_back(maxi);
+        }
         mn[n - 1] = nums[n - 1];
-        for(int i = n - 2; i >= 0; i--)
-            mn[i] = min(mn[i + 1], nums[i]);
-        
-        int maxi = nums[0];
-        for(int i = 0; i < n; i++){
-            maxi = max(maxi, nums[i]);
-            if(maxi - mn[i] <= k) return i;
+        for(int i = n - 2; i >= 0; i--) {
+            mn[i] = min(nums[i], mn[i + 1]);
+        }
+
+        if(mn[0] == mx[n - 1]) return 0;
+
+        for (int i = 0; i < n; ++i) {
+            int score = mx[i] - mn[i];
+            if (score <= k) {
+                return i;
+            }
         }
         return -1;
     }
